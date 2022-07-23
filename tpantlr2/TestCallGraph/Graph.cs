2d5be71746740fb03536c00407a0e27b2fa1f29d
Philippe Laval <philippe.laval.1968@laposte.net>
2022-07-24 00:03:00 +0200

    Antlr4BuildTasks

--- tpantlr2/TestCallGraph/Graph.cs
+++ tpantlr2/TestCallGraph/Graph.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Antlr4.StringTemplate;
+//using Antlr4.StringTemplate;
 using Antlr4.Runtime.Misc;
 
 namespace TestCallGraph
